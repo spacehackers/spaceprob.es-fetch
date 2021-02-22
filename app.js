c7aa2ebd953bb3e1febc3f44135101694b0a410d
Matt Biddulph <mbiddulph@apple.com>
2021-02-21 18:40:56 -0800

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+const fetch =  require('node-fetch');
+var ApiBuilder = require('claudia-api-builder'),
+  api = new ApiBuilder();
+
+module.exports = api;
+
+api.get('/solo', function (request) {
+  return fetch('http://estracknow.esa.int/live/mission/SOLO').then(res => res.json())
+},{
+  success: { contentType: 'application/json', headers: { "Cache-Control": "max-age=1800" }  }
+});
